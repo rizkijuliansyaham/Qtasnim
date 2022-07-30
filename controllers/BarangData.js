e700927a85099c979deff34c2a571930eb91d0f0
rizkijuliansyaham <rizki.juliansyah.a.m@gmail.com>
2022-07-30 15:59:53 +0700

    CRUD Barang

--- controllers/BarangData.js
+++ controllers/BarangData.js
@@ -8,5 +8,64 @@ export const getDataBarang = async (req,res) => {
     } catch (error) {
         res.json({message: error.message});
     }
+};
+
+
+export const updateDataBarang = async (req,res) => {
+    try {
+        await BarangData.update(req.body, {
+            where: {
+                nama_barang: req.params.nama_barang,
+            },
+        });
+        res.json({
+            message: "data update",
+        });
+    } catch (error) {
+        res.json({message: error.message});
+    }
+};
+
+
+export const getBarangById = async (req, res) => {
+    try {
+        const datas = await BarangData.findAll({
+            where: {
+                nama_barang: req.params.nama_barang,
+            },
+        });
+        res.send(datas[0]);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const createDataBarang = async (req, res) => {
+    try {
+        await BarangData.create(req.body);
+        res.json({
+            "message" : "Barang dibuat"
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const deleteBarang = async (req, res) => {
+    try {
+        await BarangData.destroy({
+            where: {
+                nama_barang: req.params.nama_barang
+            }
+        });
+        res.json({
+            message: "Barang dihapus"
+        });
+    } catch (error) {
+        
+    }
 }
+
+
+
 
--- routes/barangRoutes.js
+++ routes/barangRoutes.js
@@ -2,9 +2,16 @@ import express from "express";
 
 import {
     getDataBarang,
+    updateDataBarang,
+    createDataBarang,
+    getBarangById,
+    deleteBarang
 } from "../controllers/BarangData.js";
 
 const BarangRouter = express.Router();
 BarangRouter.get("/", getDataBarang);
-
+BarangRouter.post("/update/:nama_barang", updateDataBarang);
+BarangRouter.get("/:nama_barang", getBarangById);
+BarangRouter.post("/create", createDataBarang);
+BarangRouter.delete("/delete/:nama_barang", deleteBarang);
 export default BarangRouter;
