8193884bf9bf2ad58aa9c0b36b9b3b9427e9a579
rizkijuliansyaham <rizki.juliansyah.a.m@gmail.com>
2022-07-30 16:10:41 +0700

    CRUD Jenis

--- controllers/JenisData.js
+++ controllers/JenisData.js
@@ -8,4 +8,61 @@ export const getDataJenis = async (req,res) => {
     } catch (error) {
         res.json({message: error.message});
     }
+};
+
+
+
+export const updateDataJenis = async (req,res) => {
+    try {
+        await JenisData.update(req.body, {
+            where: {
+                id_jenis: req.params.id_jenis,
+            },
+        });
+        res.json({
+            message: "data update",
+        });
+    } catch (error) {
+        res.json({message: error.message});
+    }
+};
+
+
+export const getJenisById = async (req, res) => {
+    try {
+        const datas = await JenisData.findAll({
+            where: {
+                id_jenis: req.params.id,
+            },
+        });
+        res.send(datas[0]);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const createDataJenis = async (req, res) => {
+    try {
+        await JenisData.create(req.body);
+        res.json({
+            "message" : "Jenis dibuat"
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const deleteJenis = async (req, res) => {
+    try {
+        await BarangData.destroy({
+            where: {
+                id_jenis: req.params.id
+            }
+        });
+        res.json({
+            message: "Jenis dihapus"
+        });
+    } catch (error) {
+        
+    }
 }
--- routes/jenisRoutes.js
+++ routes/jenisRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
 
 import {
-    getDataJenis,
+    createDataJenis,
+    deleteJenis,
+    getDataJenis, getJenisById, updateDataJenis,
 } from "../controllers/JenisData.js";
 
 const JenisRouter = express.Router();
 JenisRouter.get("/", getDataJenis);
+JenisRouter.post("/update/:id_jenis", updateDataJenis);
+JenisRouter.get("/:id_jenis", getJenisById);
+JenisRouter.post("/create", createDataJenis);
+JenisRouter.delete("/delete/:id_jenis", deleteJenis);
 // TransaksiRouter.post("/:id_transaksi", update)
 
 export default JenisRouter;
